9b560e631f5d86f4f1f3cd55204a13bb5361cc8a
bae dong hwan <bdhwan@gmail.com>
2020-12-23 05:00:30 +0900

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,44 +2,41 @@ import { Controller, Get, Res, Req } from "@nestjs/common";
 import { AppService } from "./app.service";
 
 const exec = require("child_process").exec;
-// const Gpio = require("onoff").Gpio; // Gpio class
-// const led = new Gpio(21, "out");
+const Gpio = require("onoff").Gpio; // Gpio class
+const led = new Gpio(21, "out");
 
 @Controller()
 export class AppController {
   // ps -ef | grep -v grep | grep test-launch
 
   constructor(private readonly appService: AppService) {
-    this.startVideo();
+    this.startServer();
   }
 
-  async startVideo() {
-    await this.stopVideo();
-
-    // const ledResult = await led.read();
-    // console.log("ledResult", ledResult);
-    // await led.write(1);
-
+  async startServer() {
+    const ledResult = await led.read();
+    console.log("ledResult", ledResult);
+    await led.write(1);
+  }
+  async startVideo(ip: string) {
     // .then(value => led.write(value ^ 1))
     // const commend = `/home/pi/gst-rtsp-server-1.14.4/examples/test-launch --gst-debug=3 "( rpicamsrc bitrate=800000  preview=false ! video/x-h264, width=1350, height=720, framerate=30/1 ! h264parse ! rtph264pay name=pay0 pt=96 )"`;
     // const commend = `/home/pi/gst-rtsp-server-1.14.4/examples/test-launch --gst-debug=3 "( rpicamsrc bitrate=800000  preview=false ! video/x-h264, width=1350, height=720, framerate=30/1 ! h264parse ! rtph264pay name=pay0 pt=96 )"`;
     // /test-launch --gst-debug=3 "( rpicamsrc bitrate=8000000 awb-mode=tungsten preview=false ! video/x-h264, width=640, height=480, framerate=30/1 ! h264parse ! rtph264pay name=pay0 pt=96 )"
 
-    // const commend = `/home/pi/gst-rtsp-server-1.14.4/examples/test-launch --gst-debug=3 "( rpicamsrc bitrate=8000000 preview=false ! video/x-h264, width=1350, height=720, framerate=30/1 ! h264parse ! rtph264pay name=pay0 pt=96 )"`;
-
-    // console.log("will start");
-    // exec(commend, function (error, stdout, stderr) {
-    //   console.log("stdout: " + stdout);
-    //   console.log("stderr: " + stderr);
-    //   if (error !== null) {
-    //     console.log("exec error: " + error);
-    //   }
-    // });
+    const commend = `raspivid -t 999999 -h 720 -w 1080 -fps 25 -b 2000000 -o - | gst-launch-1.0 -v fdsrc fd=0 ! h264parse ! rtph264pay ! udpsink host=${ip} port=5000`;
+    console.log("will start", commend);
+    exec(commend, function(error, stdout, stderr) {
+      console.log("stdout: " + stdout);
+      console.log("stderr: " + stderr);
+      if (error !== null) {
+        console.log("exec error: " + error);
+      }
+    });
   }
 
   stopVideo() {
-    // led.write(0);
-
+    led.write(0);
     return new Promise((resolve, reject) => {
       console.log("will stopVideo");
       const commend = `ps -ef | grep 'gst-launch-1.0' | grep -v grep | awk '{print $2}' | xargs -r kill -9`;
@@ -71,7 +68,7 @@ export class AppController {
   @Get("start")
   start(@Res() res) {
     console.log("will return");
-    this.startVideo();
+    // this.startVideo();
     res.json({
       check: true,
     });
@@ -97,6 +94,8 @@ export class AppController {
     if (!ip) {
       ip = req.query.ip;
     }
+    await this.stopVideo();
+    await this.startVideo(ip);
 
     res.json({
       check: true,
